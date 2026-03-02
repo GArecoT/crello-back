4ea99f19347778464a59b04868308418668a64b1
Gabriel Areco <areco23@gmail.com>
2026-03-02 10:35:11 -0400

    FIX: Verificar validade token

--- composables/verificaToken.ts
+++ composables/verificaToken.ts
@@ -2,37 +2,36 @@ import { Database } from "@db/sqlite";
 import consts from "../composables/consts.json" with { type: "json" };
 import { RespostaInterna } from "../composables/tipos.ts";
 
-export function verificaToken(token: string): RespostaInterna {
+export function verificaToken(token: string | null): RespostaInterna {
   const db = new Database(`${consts.db}.db`);
+
+  if (token == null || token?.length === 0) {
+    return {
+      status: false,
+      msg: "Token Vazio",
+      data: {},
+    };
+  }
+
+  if (token.includes("Bearer ")) {
+    token = token.replace("Bearer ", "");
+  }
 
   const tokens = db.prepare(
     `
-	SELECT * FROM tokens WHERE token = '${token}';
+	SELECT * FROM tokens WHERE token = '${token}' and expirar >= ${Date.now()};
   `,
   ).all();
 
   db.close();
 
-  if (tokens.length > 0) {
-    if (tokens[0].expirar >= Date.now()) {
-      return {
-        status: true,
-        msg: "Login feito com sucesso!",
-        data: token[0],
-      };
-    } else {
-      const db = new Database(`${consts.db}.db`);
-
-      db.exec(
-        `
-        DELETE FROM tokens WHERE token = '${token}';
-        `,
-      );
-
-      db.close();
-      return { status: false, msg: "Usuário não validado!", data: {} };
-    }
+  if (tokens?.length > 0) {
+    return {
+      status: true,
+      msg: "Token válido!",
+      data: {},
+    };
   } else {
-    return { status: false, msg: "Usuário não validado!", data: {} };
+    return { status: false, msg: "Token expirado/inválido", data: {} };
   }
 }
