a4f6c87f0178534af92071a9de5c3e71e9f4cf37
Gabriel Areco <areco23@gmail.com>
2026-03-02 10:34:34 -0400

    FIX: Hashing desnecessário

--- controllers/salvaToken.ts
+++ controllers/salvaToken.ts
@@ -1,16 +1,14 @@
 import { Database } from "@db/sqlite";
 import consts from "../composables/consts.json" with { type: "json" };
-import hash from "../composables/hash.ts";
 
 export default async function (token: string) {
   const db = new Database(`${consts.db}.db`);
 
-  const hashToken = await hash(token);
   const expirar = Date.now() + consts.expiracao_token;
 
   db.exec(
     `
-    INSERT INTO tokens (token, expirar) Values ('${hashToken}', '${expirar}')
+    INSERT INTO tokens (token, expirar) Values ('${token}', '${expirar}')
       `,
   );
 
