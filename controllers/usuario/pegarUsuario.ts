17ca98af1ce0a8859dcb25ea251f8935bfaa822c
Gabriel Areco <areco23@gmail.com>
2026-03-13 10:15:07 -0400

    CHORE: Mover cotrollers de usuário

---
+++ controllers/usuario/pegarUsuario.ts
@@ -0,0 +1,29 @@
+import { Database } from "@db/sqlite";
+import consts from "../composables/consts.json" with { type: "json" };
+import { RespostaInterna, Usuario } from "../composables/tipos.ts";
+
+export default function (usuario: Usuario, campo = "id"): RespostaInterna {
+  const db = new Database(`${consts.db}.db`);
+
+  if (campo == "id" || campo == "nome" || campo == "admin") {
+    const usuarios = db.prepare(
+      `
+	SELECT * FROM usuarios WHERE ${campo} = '${usuario[campo]}';
+  `,
+    ).all();
+
+    db.close();
+
+    if (usuarios.length > 0) {
+      return {
+        status: true,
+        msg: "Usuário encontrado!",
+        data: usuarios[0],
+      };
+    } else {
+      return { status: false, msg: "Usuário não encontrado!", data: {} };
+    }
+  } else {
+    return { status: false, msg: "Usuário não encontrado!", data: {} };
+  }
+}
