1827f4fbbffb3bf8a3e78137586e9ecbe0ec2615
Gabriel Areco <areco23@gmail.com>
2026-03-16 10:36:13 -0400

    CHORE: Remover console.log

--- composables/limparTokensVencidos.ts
+++ composables/limparTokensVencidos.ts
@@ -2,7 +2,6 @@ import { Database } from "@db/sqlite";
 import consts from "./consts.json" with { type: "json" };
 
 export default function () {
-  console.log("a");
   const db = new Database(`${consts.db}.db`);
 
   db.exec(
