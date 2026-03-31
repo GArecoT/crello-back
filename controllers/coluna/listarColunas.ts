49a07d50711f62c8cd146602158c7e6b22f8f2ba
Gabriel Areco <areco23@gmail.com>
2026-03-31 14:34:29 -0400

    CHORE: Listar colunas somente lista nome das colunas

--- controllers/coluna/listarColunas.ts
+++ controllers/coluna/listarColunas.ts
@@ -1,15 +1,15 @@
 import consts from "../../composables/consts.json" with { type: "json" };
 import { Database } from "@db/sqlite";
 import { RespostaInterna } from "../../composables/tipos.ts";
-import listarCategorias from "../cards/categorias/listarCategorias.ts";
-import listarUsuarios from "../cards/usuarios/listarUsuarios.ts";
+// import listarCategorias from "../cards/categorias/listarCategorias.ts";
+// import listarUsuarios from "../cards/usuarios/listarUsuarios.ts";
 import salvaCache from "../cache/salvaCache.ts";
 import listaCache from "../cache/listaCache.ts";
+// import pegarColuna from "./pegarColuna.ts";
 
 export default function (): RespostaInterna {
   const resCache = listaCache("listarColunas");
   if (resCache.status) {
-    console.log("tem cache");
     return {
       status: true,
       msg: "Colunas listadas com sucesso!",
@@ -24,28 +24,24 @@ export default function (): RespostaInterna {
       `,
   ).all();
 
-  colunas.forEach((val, index) => {
-    colunas[index].cards = db.prepare(
-      `
-      SELECT * FROM cards
-      WHERE id_coluna = ${val.id};
-      `,
-    ).all();
-    colunas[index].cards.forEach((val2: any, index2: number) => {
-      const categorias = listarCategorias(val2.id);
-      const usuarios = listarUsuarios(val2.id);
-      if (categorias.status) {
-        colunas[index].cards[index2].categorias = categorias.data;
-      } else {
-        colunas[index].cards[index2].categorias = [];
-      }
-      if (usuarios.status) {
-        colunas[index].cards[index2].usuarios = usuarios.data;
-      } else {
-        colunas[index].cards[index2].usuarios = [];
-      }
-    });
-  });
+  // colunas.forEach((val, index) => {
+  //   console.log(pegarColuna(colunas[index]));
+  //   colunas[index].cards = pegarColuna(colunas[index]).data.cards;
+  //   colunas[index].cards.forEach((val2: any, index2: number) => {
+  //     const categorias = listarCategorias(val2.id);
+  //     const usuarios = listarUsuarios(val2.id);
+  //     if (categorias.status) {
+  //       colunas[index].cards[index2].categorias = categorias.data;
+  //     } else {
+  //       colunas[index].cards[index2].categorias = [];
+  //     }
+  //     if (usuarios.status) {
+  //       colunas[index].cards[index2].usuarios = usuarios.data;
+  //     } else {
+  //       colunas[index].cards[index2].usuarios = [];
+  //     }
+  //   });
+  // });
   db.close();
   salvaCache({ servico: "listarColunas", json: colunas });
   return {
